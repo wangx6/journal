e6b6958a6e9786e658ba56c9f3260f8a4ccfe514
Xin Wang <xin@xwerx.com>
2014-11-05 10:51:17 +0000

    path complete

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script type="text/javascript" src="js/app/article.js"></script>
+		<script type="text/javascript" src="js/app/river.js"></script>
+	</body>
+</html>
